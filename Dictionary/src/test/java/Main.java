ff074b48fb317d5f5bd98ed9b0b86a8cff9c319a
Julio Renner <julio.renner@hotmail.com>
2016-06-17 18:00:29 -0300

    Missing Comments

--- Dictionary/src/test/java/Main.java
+++ Dictionary/src/test/java/Main.java
@@ -6,24 +6,45 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-
         Translator translator = new Translator();
 
-        System.out.println("Enter the word in want to translate: ");
-        try{
+        System.out.println("Enter the word you want to translate: ");
+        try {
             BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-            ArrayList<String> words = translator.translateWord(bufferRead.readLine());
+            String key = bufferRead.readLine();
+            ArrayList<String> words = translator.translateWord(key);
 
-            if (words.isEmpty())
-                System.out.println("Searched word was not found in Dictionary, do you want to insert it?");
+            if (words.isEmpty()) {
+                System.out.println("Searched word not found in Dictionary, do you want to insert it? (y/n)");
 
-            else
-                for (String word: words)
+                if ((bufferRead.readLine()).equals("y")) {
+                    ArrayList<String> translations = new ArrayList<String>();
+
+                    String keepExecuting;
+                    do {
+                        System.out.println("Insert translation:");
+                        try {
+                            translations.add(bufferRead.readLine());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.print("Do you want to insert more words? (y/n)");
+
+                        keepExecuting = bufferRead.readLine();
+                    } while (keepExecuting.equals("y"));
+
+                    translator.insertTranslation(key, translations);
+                    System.out.println("Following words were inserted: Word - " + key +
+                            " Translations: " + translator.translateWord(key));
+                }
+
+            }else{
+                for (String word : words)
                     System.out.println(word);
-        }
-        catch(IOException ex)
-        {
-            ex.printStackTrace();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
